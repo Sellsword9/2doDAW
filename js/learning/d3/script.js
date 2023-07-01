028c4ade8274f016e19f0098bc5caf15f53cc048
Sellsword9 <yerayromeroortiz@gmail.com>
2023-07-01 16:26:36 +0200

    Auto-detect darkmode

--- js/learning/d3/script.js
+++ js/learning/d3/script.js
@@ -1,3 +1,12 @@
 function toggleColorScheme() {
     document.body.classList.toggle('dark-mode');
   }
+  
+  function checkSystemColorScheme() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.classList.add('dark-mode');
+    }
+  }
+  
+  // Call the function to check the system color scheme on page load
+  checkSystemColorScheme();
