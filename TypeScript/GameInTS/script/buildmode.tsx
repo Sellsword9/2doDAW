bdde8bb4e8e50f24969670b0b4f302d8e1f3b083
Sellsword9 <yerayromeroortiz@gmail.com>
2023-07-15 20:39:54 +0200

    ClassesWIP1

--- TypeScript/GameInTS/script/buildmode.tsx
+++ TypeScript/GameInTS/script/buildmode.tsx
@@ -40,3 +40,58 @@ function updateStats() {
     element.addEventListener("input", updateStats); // Añadir un listener para cuando se actualice
     // Que llame a la función updateStats
 });
+
+// Crear preview
+class previewCardModel
+{
+    Fuerza: number;
+    Const: number;
+    Dex: number;
+    Intel: number;
+    Carisma: number;
+    Suerte: number;
+    // Atributos
+    hp:  number; // Vida
+    mag: number;    dmg: number;   vc: number; //Ataques
+    def: number;    eva: number;   sp: number; //Otros
+    //Modificadores (F.Mod(stats) -> atts)
+    // Fuerza
+    DmgFrz: number = +4;
+    DefFrz: number = +2;
+    HpFrz:  number = +1;
+    SpFrz:  number = -1;
+    // Constitución
+    DefConst: number = +4;
+    HpConst:  number = +2;
+    SpConst:  number = +1;
+    EvaConst: number = -1;
+    // Destreza
+    EvaDex: number = +4;
+    DmgDex: number = +2;
+    SpDex:  number = +1;
+    HpDex:  number = -1;
+    // Inteligencia
+    MagIntel: number = +4;
+    SpIntel:  number = +2;
+    EvaIntel: number = +1;
+    DefIntel: number = -1;
+    // Carisma
+    VcChar:  number = +4;
+    MagChar: number = +1;
+    DmgChar: number = +1;
+    // Suerte
+    EvaSuerte: number = +2;
+    VcSuerte:  number = +2;
+    SpSuerte:  number = +1;
+    DmgSuerte: number = +1; 
+
+    constructor(frz: number, con: number, dex: number, 
+                int: number, chr: number, srt: number)
+    {
+        this.Fuerza = frz; this.Const = con; this.Dex = dex;
+        this.Intel = int; this.Carisma = chr; this.Suerte = srt;
+        // Asignación atributos
+
+
+    }
+}
