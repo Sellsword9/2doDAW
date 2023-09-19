a0382449b7e263a7513746064ba1f641e58bf07e
Sellsword9 <yerayromeroortiz@gmail.com>
2023-09-19 18:00:50 +0200

    Conceptual test 1 working, need to invoke real pi

--- pruebas de proyecto/prueba1/script.js
+++ pruebas de proyecto/prueba1/script.js
@@ -1,13 +1,16 @@
+// script.js
 const canvas = document.getElementById("mathCanvas");
 const ctx = canvas.getContext("2d");
 const equationInput = document.getElementById("equationInput");
 
 let t = 0;
-let equation = "sin(t)";
+let equation = "3.1415 * Math.sin(t * 0.5)";
+const trace = [];
 
 function updateEquation() {
     equation = equationInput.value;
     clearCanvas();
+    trace.length = 0; // Clear the trace when the equation changes
     animate();
 }
 
@@ -18,18 +21,37 @@ function clearCanvas() {
 function animate() {
     clearCanvas();
     
-    // Update the equation and plot points
-    for (let i = 0; i < canvas.width; i++) {
-        const x = i;
-        const y = eval(equation);
-        
-        ctx.fillStyle = "#FF0000";
-        ctx.fillRect(x, canvas.height / 2 - y * 50, 2, 2);
+    const centerX = canvas.width / 2;
+    const centerY = canvas.height / 2;
+    const radius = 100;
+
+    const x = centerX + radius * Math.cos(eval(equation));
+    const y = centerY + radius * Math.sin(eval(equation));
+
+    trace.push({ x, y });
+
+    ctx.strokeStyle = "#FF0000";
+    ctx.beginPath();
+
+    for (let i = 0; i < trace.length; i++) {
+        const point = trace[i];
+        ctx.lineTo(point.x, point.y);
     }
-    
-    t += 0.1; // Increment time for animation
-    
+
+    ctx.stroke();
+
+    ctx.fillStyle = "#FF0000";
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, Math.PI * 2);
+    ctx.fill();
+
+    t += 0.01; // Increment time for animation
+
+    if (trace.length > 2000) {
+        trace.shift();
+    }
+
     requestAnimationFrame(animate);
 }
 
-animate(); // Start the animation
+animate();
