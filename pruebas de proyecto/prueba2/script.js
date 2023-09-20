5bf064bd30afed6df76dd42e33c063d975617fad
Sellsword9 <yerayromeroortiz@gmail.com>
2023-09-20 14:43:29 +0200

    trying something

--- pruebas de proyecto/prueba2/script.js
+++ pruebas de proyecto/prueba2/script.js
@@ -19,10 +19,10 @@ function drawEquation() {
         const compiledExpression = parsedExpression.compile();
 
         // Set the number of points for a smoother curve
-        const numPoints = equationCanvas.width;
+        const numPoints = 8000;
 
         // Define the x-axis and y-axis scale
-        const xScale = 1; // Adjust as needed for horizontal scaling
+        let xScale = 1; // Adjust as needed for horizontal scaling
         const yScale = 800; // Adjust as needed for vertical scaling
         
         // Wait this works
@@ -30,8 +30,12 @@ function drawEquation() {
         // Draw the equation plot with lines
         ctx.beginPath();
         for (let i = 0; i < numPoints; i++) {
+            // increase x scale depending on the potency of the equation
+            for (let j = 0; j < parsedExpression.filter((node) => node.isSymbolNode && node.name === 'x').length; j++) {
+                xScale *= 5 ^ j ;
+            }
             const x = (i - equationCanvas.width / 2) * xScale / equationCanvas.width;
-            const y = compiledExpression.evaluate({ x: x });
+            const y = compiledExpression.evaluate({ x: x});
             const canvasX = i;
             const canvasY = equationCanvas.height / 2 - y * yScale;
             if (i === 0) {
