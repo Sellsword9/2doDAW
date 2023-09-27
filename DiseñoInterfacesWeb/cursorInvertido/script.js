1a0453b6d5933aab267695045396e3fe8e56a6e4
Sellsword9 <yerayromeroortiz@gmail.com>
2023-09-27 14:26:40 +0200

    Added dark mode just for fun

--- DiseñoInterfacesWeb/cursorInvertido/script.js
+++ DiseñoInterfacesWeb/cursorInvertido/script.js
@@ -9,6 +9,26 @@ document.addEventListener('mousemove', (e) => {
 });
 
 
+function toggleColorScheme() {
+    document.body.classList.toggle('dark-mode');
+  }
+
+function checkSystemColorScheme() {
+    console.log('checkSystemColorScheme() called');
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      document.body.classList.add('dark-mode');
+      document.body.classList.toggle('dark-mode');
+      console.log('checkSystemColorScheme() toggled');
+    }
+    else
+    {
+        console.log('checkSystemColorScheme() not toggled');
+    }
+  }
+
+  // Call the function to check the system color scheme on page load
+  checkSystemColorScheme();
+
 
 
 
