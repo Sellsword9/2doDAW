d8f45c8b5c33af50b29810f25a5926a0122edf1e
Sellsword9 <yerayromeroortiz@gmail.com>
2023-10-05 13:24:05 +0200

    Second function

--- DevEntCliente/GestionUser/script.js
+++ DevEntCliente/GestionUser/script.js
@@ -6,4 +6,17 @@ const functio  = (data) => {
         .reduce((mapa, obj) => mapa.set(`${obj.first_name} ${obj.last_name}`, obj.email), new Map());
 }
 
+const espanioles = (data) => {
+    arr
+        .reduce((mapa, obj) => {
+            obj.email.slice(-3) === ".es" 
+              ?  mapa.set(`${obj.last_name}_${user.first_name}`, obj.email) 
+                : mapa
+            
+            
+            
+            ,new Map()});
+}
+
+
 console.log(espanioles(data))
